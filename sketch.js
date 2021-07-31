32d8a16b879723d070b64ea2f15bde9a6a913d77
SHUBHAM SUDAM MHASKE <85775945+Shubham392007@users.noreply.github.com>
2021-07-31 10:38:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,11 +22,11 @@ function setup() {
 	bob3 = new Bob (420,470,20)
 	bob4 = new Bob (460,470,20)
 	bob5 = new Bob (500,470,20);
-	rope1= new Rope(bob1.body,roof.body,-40*2, 0);
-	rope2= new Rope(bob2.body,roof.body,-20*2,0);
-	rope3= new Rope(bob3.body,roof.body,-0*2,0);
-	rope4= new Rope(bob4.body,roof.body,20*2,0);
-	rope5= new Rope(bob5.body,roof.body,40*2,0);
+	rope1= new Rope(bob1.body,roof.body,-80, 0);
+	rope2= new Rope(bob2.body,roof.body,-40,0);
+	rope3= new Rope(bob3.body,roof.body,0,0);
+	rope4= new Rope(bob4.body,roof.body,40,0);
+	rope5= new Rope(bob5.body,roof.body,80,0);
 	Engine.run(engine);
   
 
